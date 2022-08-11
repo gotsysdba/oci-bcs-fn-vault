f71e26f1562ecc873e2996183b76406096938122
John Lathouwers <john.lathouwers@oracle.com>
2022-08-11 12:56:36 +0100

    Lockdown Security List

--- terraform/vcn.tf
+++ terraform/vcn.tf
@@ -36,3 +36,61 @@ resource "oci_core_subnet" "subnet_private" {
   dns_label                  = "priv"
   prohibit_public_ip_on_vnic = true
 }
+
+resource "oci_core_default_security_list" "default_security_list" {
+  compartment_id = var.compartment_ocid
+  display_name = "Default Security List for bcs1-vcn"
+  egress_security_rules {
+    description      = "Access to Object Storage for Backup Cloud Service"
+    destination      = "134.70.0.0/16"
+    destination_type = "CIDR_BLOCK"
+    protocol  = "6"
+    stateless = "false"
+    tcp_options {
+      max = "443"
+      min = "443"
+    }
+  }
+  egress_security_rules {
+    description      = "Access to Bastion Tunnel for SSH Access"
+    destination      = var.vcn_cidr
+    destination_type = "CIDR_BLOCK"
+    protocol  = "6"
+    stateless = "false"
+    tcp_options {
+      max = "22"
+      min = "22"
+    }
+  }
+  ingress_security_rules {
+    protocol    = "6"
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    stateless   = "false"
+    tcp_options {
+      max = "22"
+      min = "22"
+    }
+  }
+  ingress_security_rules {
+    icmp_options {
+      code = "4"
+      type = "3"
+    }
+    protocol    = "1"
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    stateless   = "false"
+  }
+  ingress_security_rules {
+    icmp_options {
+      code = "-1"
+      type = "3"
+    }
+    protocol    = "1"
+    source      = var.vcn_cidr
+    source_type = "CIDR_BLOCK"
+    stateless   = "false"
+  }
+  manage_default_resource_id = oci_core_vcn.vcn.default_security_list_id
+}
